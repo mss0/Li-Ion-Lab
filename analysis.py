00afd797a28e86e34b09f89797b7ba2de20629da
mss0 <96515397+mss0@users.noreply.github.com>
2025-10-01 00:26:47 +0200

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
+import scipy, matplotlib
 import matplotlib.pyplot as plt
 from math import log
 from datetime import datetime
+from scipy.optimize import curve_fit
 
 def timecode_to_seconds(timecode, framerate):
 
@@ -17,7 +20,7 @@ def timecode_to_seconds(timecode, framerate):
 
 framerate = 29.97
 
-mode = 'thin_discharge' # choose which process to study
+mode = 'thick_discharge' # choose which process to study
 
 data_path = mode.split('_')
 data_path = './data/'+ data_path[0] + ' ' + data_path[1] + '.xlsx'
@@ -44,17 +47,40 @@ start_time = data.at[start_index[mode], 'time']
 # set the start time of the measurements as 0
 data['time'] = data['time'].map(lambda x: x - start_time)
 
+# curve fitting model 1
+t_data = data['time'].to_numpy()
+I_data = data['current'].to_numpy()
+
+def model(I, b1, b2):
+    return b1 + b2 / I
+
+popt, pcov = curve_fit(model, I_data, t_data)
+b1_fit, b2_fit = popt
+
+I_fit = np.linspace(min(I_data), max(I_data), 200)
+t_fit = model(I_fit, b1_fit, b2_fit)
+
+plt.scatter(t_data, I_data, label="Data", color="red")
+plt.plot(t_fit, I_fit, label="Fit", color="blue")
+plt.xlabel("Time")
+plt.ylabel("Current")
+plt.legend()
+plt.show()
+
+# plotting raw
 fig = data.plot(x='time', y='current', kind='scatter')
 fig.set_xlabel('Time')
 fig.set_ylabel('Current')
 plt.show()
 
-data.plot(x='time', y='log_current', kind='scatter')
+# plotting exponential decay attempt
+fig = data.plot(x='time', y='log_current', kind='scatter')
 fig.set_xlabel('Time')
 fig.set_ylabel('ln(Current)')
 plt.show()
 
-data.plot(x='log_time', y='log_current', kind='scatter')
+# plotting peukert attempt
+fig = data.plot(x='log_time', y='log_current', kind='scatter')
 fig.set_xlabel('ln(Time)')
 fig.set_ylabel('ln(Current)')
 plt.show()
